e293b52dd72df2b86fb3f478385f8b576c6042e8
Umarocks <umarocks@hotmail.com>
2023-11-02 16:28:23 -0500

    winning condition brute force logic working. now i will work on making the code more readable

--- Script.js
+++ Script.js
@@ -1,29 +1,67 @@
-let playerChoice="X";
+var playerChoice = "X";
+let counter = 0;
 
-function playerChoiceClick(element){
-    //make a funtion to start a new game whenever this is invoked to avoid cheating and glitches
-    playerChoice=element.textContent;
-    console.log(playerChoice);
-} 
-let counter=0;
+function playerChoiceClick(element) {
+  //make a funtion to start a new game whenever this is invoked to avoid cheating and glitches
+  playerChoice = element.textContent;
+  console.log(playerChoice);
+//   resetBoard();
+}
 
-function inputOnBoard(element){
-    const buttonId = element.id;
-    const buttonContent = document.getElementById(buttonId);
-    if(buttonContent.textContent.trim()===''){
-        buttonContent.innerHTML=playerChoice;
-        counter++;
+function inputOnBoard(element) {
+  const buttonId = element.id;
+  const buttonContent = document.getElementById(buttonId);
+  if (buttonContent.textContent.trim() === "") {
+    buttonContent.innerHTML = playerChoice;
+    counter++;
+  }
+  CheckLogic();
+}
+
+function CheckLogic() {
+    const allButtonsOnBoardCheck = document.querySelector(".map").querySelectorAll("button");
+    const a = allButtonsOnBoardCheck[1];
+    if(allButtonsOnBoardCheck[0].textContent.trim().toLowerCase()===playerChoice.trim().toLowerCase()&&allButtonsOnBoardCheck[1].textContent.trim().toLowerCase()===playerChoice.trim().toLowerCase()&&allButtonsOnBoardCheck[2].textContent.trim().toLowerCase()===playerChoice.trim().toLowerCase()){
+        console.log("PLAYER WON");
     }
-    console.log(counter);
-    // console.log(buttonContent.textContent);
+    else if(allButtonsOnBoardCheck[3].textContent.trim().toLowerCase()===playerChoice.trim().toLowerCase()&&allButtonsOnBoardCheck[4].textContent.trim().toLowerCase()===playerChoice.trim().toLowerCase()&&allButtonsOnBoardCheck[5].textContent.trim().toLowerCase()===playerChoice.trim().toLowerCase()){
+        console.log("PLAYER WON");
+    }
+    else if(allButtonsOnBoardCheck[6].textContent.trim().toLowerCase()===playerChoice.trim().toLowerCase()&&allButtonsOnBoardCheck[7].textContent.trim().toLowerCase()===playerChoice.trim().toLowerCase()&&allButtonsOnBoardCheck[8].textContent.trim().toLowerCase()===playerChoice.trim().toLowerCase()){
+        console.log("PLAYER WON");
+    }
+    else if(allButtonsOnBoardCheck[0].textContent.trim().toLowerCase()===playerChoice.trim().toLowerCase()&&allButtonsOnBoardCheck[3].textContent.trim().toLowerCase()===playerChoice.trim().toLowerCase()&&allButtonsOnBoardCheck[6].textContent.trim().toLowerCase()===playerChoice.trim().toLowerCase()){
+        console.log("PLAYER WON");
+    }
+    else if(allButtonsOnBoardCheck[1].textContent.trim().toLowerCase()===playerChoice.trim().toLowerCase()&&allButtonsOnBoardCheck[4].textContent.trim().toLowerCase()===playerChoice.trim().toLowerCase()&&allButtonsOnBoardCheck[7].textContent.trim().toLowerCase()===playerChoice.trim().toLowerCase()){
+        console.log("PLAYER WON");
+    }
+    else if(allButtonsOnBoardCheck[2].textContent.trim().toLowerCase()===playerChoice.trim().toLowerCase()&&allButtonsOnBoardCheck[5].textContent.trim().toLowerCase()===playerChoice.trim().toLowerCase()&&allButtonsOnBoardCheck[8].textContent.trim().toLowerCase()===playerChoice.trim().toLowerCase()){
+        console.log("PLAYER WON");
+    }
+    else if(allButtonsOnBoardCheck[0].textContent.trim().toLowerCase()===playerChoice.trim().toLowerCase()&&allButtonsOnBoardCheck[4].textContent.trim().toLowerCase()===playerChoice.trim().toLowerCase()&&allButtonsOnBoardCheck[8].textContent.trim().toLowerCase()===playerChoice.trim().toLowerCase()){
+        console.log("PLAYER WON");
+    }
+    else if(allButtonsOnBoardCheck[2].textContent.trim().toLowerCase()===playerChoice.trim().toLowerCase()&&allButtonsOnBoardCheck[4].textContent.trim().toLowerCase()===playerChoice.trim().toLowerCase()&&allButtonsOnBoardCheck[6].textContent.trim().toLowerCase()===playerChoice.trim().toLowerCase()){
+        console.log("PLAYER WON");
+    }
+    
 }
 
 
 
-function resetBoard(){
-    const allButtonsOnBoard = document.querySelector('.map').querySelectorAll("button");
-    allButtonsOnBoard.forEach(element => {
-        element.textContent='';
-    });
-    counter=0;
+
+
+
+function resetBoard() {
+  const allButtonsOnBoard = document
+    .querySelector(".map")
+    .querySelectorAll("button");
+  allButtonsOnBoard.forEach((element) => {
+    element.textContent = "";
+  });
+  counter = 0;
 }
+
+
+
