2f5cea163497be4c29f362f222426b48f8810f3e
Umarocks <umarocks@hotmail.com>
2023-11-02 14:11:35 -0500

    added restart funtion that resets the board

--- Script.js
+++ Script.js
@@ -1,17 +1,29 @@
-let playerChoice="";
+let playerChoice="X";
 
 function playerChoiceClick(element){
     //make a funtion to start a new game whenever this is invoked to avoid cheating and glitches
     playerChoice=element.textContent;
     console.log(playerChoice);
 } 
+let counter=0;
 
-function inputOnMap(element){
+function inputOnBoard(element){
     const buttonId = element.id;
     const buttonContent = document.getElementById(buttonId);
     if(buttonContent.textContent.trim()===''){
         buttonContent.innerHTML=playerChoice;
-
+        counter++;
     }
+    console.log(counter);
     // console.log(buttonContent.textContent);
+}
+
+
+
+function resetBoard(){
+    const allButtonsOnBoard = document.querySelector('.map').querySelectorAll("button");
+    allButtonsOnBoard.forEach(element => {
+        element.textContent='';
+    });
+    counter=0;
 }
